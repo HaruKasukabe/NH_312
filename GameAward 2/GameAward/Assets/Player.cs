50b64e1fdff88bebe98647e326e427210b415d65
knbdlbvzkijgdsoijpgr <95602015+knbdlbvzkijgdsoijpgr@users.noreply.github.com>
2022-03-26 08:08:30 +0900

    変更のされていないコミットがあったのでプッシュ

---
+++ GameAward 2/GameAward/Assets/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject ink;
+//    Vector3 createPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(0, -0.01f, 0);
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(0, 0.01f, 0);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(-0.01f, 0, 0);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(0.01f, 0, 0);
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(ink, transform.position, Quaternion.identity);
+        }
+    }
+}
